2db701fcf54224b6790719d8cbf3807ff92bae29
rasikag <rasikagayan90@gmail.com>
2018-03-24 14:15:05 +0800

    added google auth 20

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://rasikag:rasikag@ds223019.mlab.com:23019/story_board_v001',
+    googleClientID: '988707192598-pivadpam1dkvci3cthoooas3el3fuap1.apps.googleusercontent.com',
+    googleClientSecret: 'XrnhppJhjmTsnQfPKx5XvjEs'
+}
