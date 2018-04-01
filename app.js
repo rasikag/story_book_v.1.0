4b32925d335f67053da01bdc8c875a308dce2249
rasikag <rasikagayan90@gmail.com>
2018-04-01 10:50:01 +0800

    fixed the loading error

--- app.js
+++ app.js
@@ -1,60 +1,62 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const session = require('express-session')
+const session = require('express-session');
 const passport = require('passport');
 
-// load user model
+// Load User Model
 require('./models/User');
 
-// passport config 
+// Passport Config
 require('./config/passport')(passport);
 
-// load routes
+// Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
 
-//load keys
+// Load Keys
 const keys = require('./config/keys');
-// Map global promise
+
+// Map global promises
 mongoose.Promise = global.Promise;
-// connect mongoose 
+// Mongoose Connect
 mongoose.connect(keys.mongoURI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
-// Handlebars middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
 app.set('view engine', 'handlebars');
 
-// cookie parser middleware
 app.use(cookieParser());
 app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
 }));
 
-// passport middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
+// Set global vars
 app.use((req, res, next) => {
-    res.locals.user = req.user || null;
+  res.locals.user = req.user || null;
+  next();
 });
 
-// use auth 
+// Use Routes
 app.use('/', index);
 app.use('/auth', auth);
-
+//app.use('/stories', stories);
 
 const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
-    console.log(`Server start in port: ${port}`);
+  console.log(`Server started on port ${port}`)
 });
