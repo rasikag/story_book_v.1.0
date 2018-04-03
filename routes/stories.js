9135d704e1025c79a47b340328763222314db11d
rasikag <rasikagayan90@gmail.com>
2018-04-03 21:19:01 +0800

    added card

--- routes/stories.js
+++ routes/stories.js
@@ -7,6 +7,7 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 router.get('/',(req, res)=>{
     Story.find({status: 'public'})
+        .populate('user')
         .then(stories => {
             res.render('stories/index', {
                 stories: stories
