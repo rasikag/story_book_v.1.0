4b5bcd84735cc5069f5f0b784643ff072e7441e2
rasikag <rasikagayan90@gmail.com>
2018-04-05 01:03:57 +0800

    basic edit function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -43,6 +44,9 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Method override middleware
+app.use(methodOverride('_method'))
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
